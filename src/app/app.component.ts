4fdd57a3d8fa1d0846f3a06abe434b3f71fd3d6e
Raj <raj.malviya000011.rm@gmail.com>
2024-10-05 15:15:15 +0530

    project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from 'src/dummy-data/dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  users=DUMMY_USERS;
   title = 'my-first-standalone-project';
 }
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent {
+ @Input('userId') userId:string='';
+ @Input('userName') userName:string='';
+ @Input('userAvatar') userAvatar:string='';
+
+}
